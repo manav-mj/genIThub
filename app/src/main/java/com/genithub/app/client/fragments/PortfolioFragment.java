911c79ee322cbb019df2ea362164f830eeea818b
Manav Jain <manav.mj.jain@gmail.com>
2017-04-12 00:49:29 +0530

    portfolio progress bar added

--- app/src/main/java/com/genithub/app/client/fragments/PortfolioFragment.java
+++ app/src/main/java/com/genithub/app/client/fragments/PortfolioFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.genithub.app.client.Portfolio;
 import com.genithub.app.client.PortfolioAdapter;
@@ -36,6 +37,8 @@ public class PortfolioFragment extends android.support.v4.app.Fragment {
     ArrayList<Portfolio> portfolioArrayList;
     PortfolioAdapter adapter;
 
+    LinearLayout progressBarLayout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -45,6 +48,8 @@ public class PortfolioFragment extends android.support.v4.app.Fragment {
 
         recyclerView = (RecyclerView) view.findViewById(R.id.portfolio_list);
 
+        progressBarLayout = (LinearLayout) view.findViewById(R.id.progressBarLayout);
+        progressBarLayout.setVisibility(View.VISIBLE);
         adapter = new PortfolioAdapter(getActivity(), portfolioArrayList);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -59,11 +64,10 @@ public class PortfolioFragment extends android.support.v4.app.Fragment {
                 for (DataSnapshot d :
                         dataSnapshot.getChildren()) {
                     Map<String, String> map = (Map<String, String>) d.getValue();
-                    Log.d("manav", "onDataChange: " + d.getValue());
-                    Log.d(TAG, "onDataChange: " + map.toString());
                     portfolioArrayList.add(new Portfolio(map.get("name"), map.get("type"), map.get("imgUrl"), ""));
                 }
                 adapter.notifyDataSetChanged();
+                progressBarLayout.setVisibility(View.INVISIBLE);
             }
 
             @Override
